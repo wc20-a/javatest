08576f3544415703121dee43c1034ce2d8e5d044
汪诚 <2216504153@qq.com>
2020-02-12 17:39:54 +0800

    第一次æ·上传Java代码

---
+++ src/com/wc/demo/test.java
@@ -0,0 +1,19 @@
+package com.wc.demo;
+
+/**
+ * @Author: 汪诚
+ * @Date: 2020/2/12 17:29
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+        System.out.println("金泰妍");
+    }
+}
